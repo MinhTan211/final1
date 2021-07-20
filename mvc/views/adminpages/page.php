f5889e013990340e0dcb3aa94ff62a53f690e992
MinhTan211 <87536020+MinhTan211@users.noreply.github.com>
2021-07-20 18:50:53 +0700

    listpage

--- mvc/views/adminpages/page.php
+++ mvc/views/adminpages/page.php
@@ -1,14 +1,34 @@
 <!--Pagination-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ví dụ Bootstrap --- dammio.com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
 <nav aria-label="Page navigation example">
-    <ul class="pagination">
-        <?php                                    
+<div class="container">
+  <ul class="pagination">
+  <li class="page-item"><a class="page-link" href="#">Trước</a></li>
+ <?php                                    
             $i =1 ;                                         
             for($i; $i <= $pages; $i++){
-                if($i == $page)
-                     echo '<span>'.$page.'</span>';
+                if($i == $page){
+                    echo "<li class='page-item'><a class='page-link' href='#'>$page</a></li>";
+                }
                 else
-                    echo '<a href="./'.$PageName.'?page='.$i.'">'.$i.'</a>';
+                echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.$i.'">'.$i.'</a></li>';
+                
             }
         ?>
-    </ul>
+    <li class="page-item"><a class="page-link" href="#">Sau</a></li>
+  </ul>
+</div>
 </nav>
+</body>
+</html>
