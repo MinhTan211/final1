af86d98ac2dd9356ed00169f6a21c794742de92a
MinhTan211 <87536020+MinhTan211@users.noreply.github.com>
2021-07-20 18:59:56 +0700

    edit listpage

--- mvc/views/adminpages/page.php
+++ mvc/views/adminpages/page.php
@@ -1,16 +1,3 @@
-<!--Pagination-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Ví dụ Bootstrap --- dammio.com</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
 <nav aria-label="Page navigation example">
 <div class="container">
   <ul class="pagination">
@@ -19,7 +6,7 @@
             $i =1 ;                                         
             for($i; $i <= $pages; $i++){
                 if($i == $page){
-                    echo "<li class='page-item'><a class='page-link' href='#'>$page</a></li>";
+                    echo "<li class='page-item active'><a class='page-link' href='#'>$page</a></li>";
                 }
                 else
                 echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.$i.'">'.$i.'</a></li>';
@@ -30,5 +17,3 @@
   </ul>
 </div>
 </nav>
-</body>
-</html>
