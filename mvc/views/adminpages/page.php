38f5ee76e40ea722bdf25e52095176271ea8184d
MinhTan211 <87536020+MinhTan211@users.noreply.github.com>
2021-07-20 19:16:50 +0700

    broad listcustomer

--- mvc/views/adminpages/page.php
+++ mvc/views/adminpages/page.php
@@ -1,12 +1,12 @@
 <nav aria-label="Page navigation example">
-<div class="container">
+<div class="container" style="margin-left: 500px;">
   <ul class="pagination">
   <li class="page-item"><a class="page-link" href="#">Trước</a></li>
  <?php                                    
             $i =1 ;                                         
             for($i; $i <= $pages; $i++){
                 if($i == $page){
-                    echo "<li class='page-item active'><a class='page-link' href='#'>$page</a></li>";
+                    echo "<li class='page-item active' ><a class='page-link' href='#'>$page</a></li>";
                 }
                 else
                 echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.$i.'">'.$i.'</a></li>';
