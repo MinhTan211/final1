cf03c3bd3686c62c00cd2fce080f084e8317ae94
linh228 <72004972+linh228@users.noreply.github.com>
2021-07-20 19:22:50 +0700

    Truoc Sau

--- mvc/views/adminpages/page.php
+++ mvc/views/adminpages/page.php
@@ -14,19 +14,27 @@
 <nav aria-label="Page navigation example">
 <div class="container">
   <ul class="pagination">
-  <li class="page-item"><a class="page-link" href="#">Trước</a></li>
+    <?php
+      if($page<=1)
+        echo "<li class='page-item'><span class='page-link'>Trước</span></li>";
+      else
+      echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.($page-1).'">Trước</a></li>';
+    ?>
  <?php                                    
             $i =1 ;                                         
             for($i; $i <= $pages; $i++){
-                if($i == $page){
-                    echo "<li class='page-item'><a class='page-link' href='#'>$page</a></li>";
-                }
+                if($i == $page)
+                    echo "<li class='page-item'><span class='page-link'>".$page."</span></li>";
                 else
-                echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.$i.'">'.$i.'</a></li>';
-                
+                  echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.$i.'">'.$i.'</a></li>';
             }
         ?>
-    <li class="page-item"><a class="page-link" href="#">Sau</a></li>
+    <?php
+      if($page>=$pages)
+        echo "<li class='page-item'><span class='page-link'>Sau</span></li>";
+      else
+        echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.($page+1).'">Sau</a></li>';
+    ?>
   </ul>
 </div>
 </nav>
