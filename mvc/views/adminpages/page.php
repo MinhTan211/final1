178a170d79b36ebf15101371ce455839c5f358c2
linh228 <72004972+linh228@users.noreply.github.com>
2021-07-21 15:32:09 +0700

    Update Page

--- mvc/views/adminpages/page.php
+++ mvc/views/adminpages/page.php
@@ -2,25 +2,52 @@
 <div class="container" style="margin-left: 500px;">
   <ul class="pagination">
     <?php
-      if($page<=1)
+      $SoTrang = 5;
+      if($SoTrang>$pages){
+        $TrangBD = 1;
+        $TrangKT = $pages;
+      }
+      else{
+        $TrangBD = $page - floor(($SoTrang-1)/2);
+        $TrangKT = $TrangBD + ($SoTrang-1);
+        if($TrangBD<1){
+          $TrangBD = 1;
+          $TrangKT = $TrangBD + ($SoTrang-1);
+        }
+        else if($TrangKT>$pages){
+          $TrangKT = $pages;
+          $TrangBD = $TrangKT - ($SoTrang-1);
+        }
+      }
+      if($page<=1){
+        echo "<li class='page-item'><span class='page-link'>Đầu</span></li>";
         echo "<li class='page-item'><span class='page-link'>Trước</span></li>";
+      }
       else
-      echo "<li class='page-item'><a class='page-link '".'href="./'.$PageName.'?page='.($page-1).'">Trước</a></li>';
+      {
+        echo "<li class='page-item'><a class='page-link '".'href="./'.$PageName.'?page=1">Đầu</a></li>';
+        echo "<li class='page-item'><a class='page-link '".'href="./'.$PageName.'?page='.($page-1).'">Trước</a></li>';
+      }
     ?>
- <?php                                    
-            $i =1 ;                                         
-            for($i; $i <= $pages; $i++){
-                if($i == $page)
+ <?php                                                                           
+            for($TrangBD; $TrangBD <= $TrangKT; $TrangBD++){
+                if($TrangBD == $page)
                     echo "<li class='page-item active'><span class='page-link '>".$page."</span></li>";
                 else
-                  echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.$i.'">'.$i.'</a></li>';
+                  echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.$TrangBD.'">'.$TrangBD.'</a></li>';
             }
         ?>
     <?php
       if($page>=$pages)
+      {
         echo "<li class='page-item'><span class='page-link'>Sau</span></li>";
+        echo "<li class='page-item'><span class='page-link'>Cuối</span></li>";
+      }
       else
+      {
         echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.($page+1).'">Sau</a></li>';
+        echo "<li class='page-item'><a class='page-link'".'href="./'.$PageName.'?page='.$pages.'">Cuối</a></li>';
+      }
     ?>
   </ul>
 </div>
