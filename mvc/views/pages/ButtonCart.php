8c47cd1fda1974d20fb51fce1210b38f796201e3
linh228 <72004972+linh228@users.noreply.github.com>
2021-07-22 22:30:18 +0700

    complete

--- mvc/views/pages/ButtonCart.php
+++ mvc/views/pages/ButtonCart.php
@@ -1,5 +1,5 @@
 <?php
-    if(isset($_SESSION['Username']))
+    if(isset($_SESSION['Username']) && isset($_SESSION['cart']))
         echo '<li>
         <a href="./shopcart"><img src="../public/img/icon/bag.png" style="height: 25px;">
             <span>Giỏ hàng</span>('.count($_SESSION["cart"]).')</a>
