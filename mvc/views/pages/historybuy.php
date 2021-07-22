b6aefeb92633a8ca8e395f55b8f37504aecc9b9b
MinhTan211 <87536020+MinhTan211@users.noreply.github.com>
2021-07-22 19:59:49 +0700

    fush

--- mvc/views/pages/historybuy.php
+++ mvc/views/pages/historybuy.php
@@ -50,7 +50,7 @@
             };
 
             reader.readAsDataURL(selectedFile);
-}
+        }
     </script>
 </head>
 
@@ -70,13 +70,13 @@
                             <li><a href="./trademark">Thương Hiệu</a></li>
                             <li><a href="./shop">Đặt Hàng</a></li>
                             <?php
-                                include 'ButtonLogout.php';
+                            include 'ButtonLogout.php';
                             ?>
                             <li>
                                 <a href="./shopcart"><img src="../public/img/icon/bag.png" style="height: 25px;"> <span>0</span> Cart: <span>$0.00</span></a>
                             </li>
                             <li class="active">
-                            <a href="./informationPersonal" style="padding-left: 20px;"><img src="../public/img/img_logo/logo1.png" style="height: 40px; width: 40px;"><label style="margin-left: 10px; font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; color: white;">TT.LT</label></a>
+                                <a href="./informationPersonal" style="padding-left: 20px;"><img src="../public/img/img_logo/logo1.png" style="height: 40px; width: 40px;"><label style="margin-left: 10px; font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; color: white;">TT.LT</label></a>
                             </li>
                         </ul>
                     </nav>
@@ -91,9 +91,9 @@
         <div class="container">
             <div class="checkout__form">
                 <form action="#" method="POST" enctype="multipart/form-data">
-                <?php
-                     $customer = $this->model('CustomerModel')->ChitietKH($_SESSION["Username"])->fetch_row();
-                ?>
+                    <?php
+                    $customer = $this->model('CustomerModel')->ChitietKH($_SESSION["Username"])->fetch_row();
+                    ?>
                     <div class="row">
                         <div class="about__text">
                             <div class="section-title">
@@ -102,112 +102,71 @@
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-6" style="text-align: center;">
-                            <img style="width: 300px; height: 360px;" src = "../uploads/<?php echo $customer[8] ?>">
+                            <img style="width: 300px; height: 360px;" src="../uploads/<?php echo $customer[8] ?>">
                             </br>
                             <div style="margin-top: 20px; padding-left: 45px;">
-                            <a style="text-align: center; margin-right: 50px;"><?php echo $customer[1] ?><span class="material-icons" style="color: black; margin-left: 10px;">
-                                edit
-                                </span></a>
+                                <a style="text-align: center; margin-right: 50px;"><?php echo $customer[1] ?><span class="material-icons" style="color: black; margin-left: 10px;">
+                                        edit
+                                    </span></a>
                             </div>
-                            
+
                             <div class="vertical-menu" style="margin-top: 30px; margin-left: 30px;">
                                 <a href="./informationPersonal" class="active">Cập Nhật Thông Tin</a>
                             </div>
                             <div class="vertical-menu" style="margin-top: 30px; margin-left: 30px;">
-                                <a href="./historybuy" class="active">Lịch Sử Mua Hàng</a>
+                                <a href="./informationPersonal" class="active">Lịch Sử Mua Hàng</a>
                             </div>
                         </div>
-                        <div class="col-lg-5 col-md-5">
-                            <div class="checkout__input">
-                                <p>Họ Và Tên<span>*</span></p>
-                                <input name="TenKH" type="text" value="<?php echo $customer[1] ?>">
-                            </div>
-                            <div class="checkout__input">
-                                <p>Địa Chỉ<span>*</span></p>
-                                <input name="Address" type="text" placeholder="Tên phường,&ensp;xã,&ensp;thành phố..." class="checkout__input__add" value="<?php echo $customer[2]?>">
-                            </div>
-                            <div class="checkout__input">
-                                <p>Email<span>*</span></p>
-                                <input name="Email" type="text" placeholder="Email..." class="checkout__input__add" <?php echo $customer[3] ?>>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="checkout__input">
-                                        <p>Số Điện Thoại<span>*</span></p>
-                                        <input name="Numberphone" type="text" value="<?php echo $customer[4] ?>">
+                        <div class="col-lg-8 col-md-8">
+                            <div class="content">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="card" style="border: 1px solid #f08632">
+                                                <div class="card-body" >
+                                                        <table class="shopping__cart__table">
+                                                            <thead class=" text-primary" style="font-family:'Courier New', Courier, monospace;">
+                                                                <th style="width: 200px; text-align: center; color: #f08632;">
+                                                                    Sản Phẩm
+                                                                </th>
+                                                                <th style="width: 230px; text-align: center; color: #f08632;">
+                                                                    Số Lượng
+                                                                </th>
+                                                                <th style="width: 230px; text-align: center; color: #f08632;">
+                                                                    Giá
+                                                                </th>
+                                                            </thead>
+                                                            <tbody>
+                                                    <tr>
+                                                        <td style="color: #f08632;" colspan="3">--------------------------------------------------------------------------------------------------------------</td>
+                                                    </tr>
+                                                    <tr style="height: 40px;">
+                                                        <td style='width: 200px;text-align: center;color: #59430b;'>22/7/2021</td>
+                                                         <td style='width: 230px;text-align: center; color: #59430b;'>3</td>
+                                                         <td style='width: 230px;text-align: center;color: #59430b;'>230000</td>
+                                                    </tr>
+                                                    <tr style="height: 40px;">
+                                                        <td style='width: 200px;text-align: center;color: #59430b;'>22/7/2021</td>
+                                                         <td style='width: 230px;text-align: center; color: #59430b;'>3</td>
+                                                         <td style='width: 230px;text-align: center;color: #59430b;'>230000</td>
+                                                    </tr>
+                                                    <tr style="height: 40px;">
+                                                        <td style='width: 200px;text-align: center;color: #59430b;'>22/7/2021</td>
+                                                         <td style='width: 230px;text-align: center; color: #59430b;'>3</td>
+                                                         <td style='width: 230px;text-align: center;color: #59430b;'>230000</td>
+                                                    </tr>
+                                                              
+                                                            </tbody>
+                                                        </table>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-lg-6">
-                                    <div class="checkout__input">
-                                        <p>Giới Tính<span>*</span></p>
-                                        <input style="width: 15px; height: 15px;" type="radio" name="gader" value="Nam"><span class="material-icons">
-                                            female
-                                            </span>
-                                        <input style="width: 15px; height: 15px; margin-left: 15px; margin-top: 15px;" type="radio" name="gader" value="Nam"><span class="material-icons">
-                                            male
-                                                </span>
-                                    </div>
-                                </div>
                             </div>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="checkout__input">
-                                        <p>Ngày Sinh<span>*</span></p>
-                                        <input type="date" id="day" name="Birthday" value="<?php echo $customer[6] ?>">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3">
-                            <a href="#">
-                                <img id="ImgCus" src="../public/img/img_logo/icon.png" style="font-size: 120px; margin-top: 10px; margin-left: 80px; height: 250px;">
-                            </a>
-                            <input name="file" type="file" onchange="onFileSelected(event)" style=" margin-top: 20px; margin-left: 80px; background: transparent; background: transparent;">
                         </div>
                     </div>
-                    <?php
-                        if($_SERVER["REQUEST_METHOD"] == "POST")
-                        {
-                            $TenKH          = "";
-                            $Address        = "";
-                            $Email          = "";
-                            $Numberphone    = "";
-                            $Gender         = "";
-                            $Birthday       = "";
-                            $ImgCus         = "";
-                            if (isset($_POST["TenKH"])) {
-                                $TenKH = $_POST['TenKH'];
-                            }
-                            if (isset($_POST["Email"])) {
-                                $Email = $_POST['Email'];
-                            }
-                            if (isset($_POST["Address"])) {
-                                $Address = $_POST['Address'];
-                            }
-                            if (isset($_POST["Numberphone"])) {
-                                $Numberphone = $_POST['Numberphone'];
-                            }
-                            if (isset($_POST["Birthday"])) {
-                                $Birthday = $_POST['Birthday'];
-                            }
 
-                            if ($_FILES['file']['name'] != NULL) { // Đã chọn file
-                                    $path = "uploads/";
-                                $tmp_name = $_FILES['file']['tmp_name'];
-                                $arr = $_FILES['file']['name'];
-                                $arr = $TenKH."_1.jpg";
-                                move_uploaded_file($tmp_name,$path.$arr);
-                            }
-                            $ImgCus = $arr;
-                            $customer = $this->model('CustomerModel')->ChitietKH($_SESSION["Username"])->fetch_row();
-                            $id             = $customer[0];
-                            $ifmation       = $this->model('CustomerModel');
-                            $ifmation->addinf($id, $TenKH, $Address, $Email, $Numberphone, $Gender, $Birthday, $ImgCus);
-                            echo "<script type='text/javascript'>
-                                    window.location = 'http://localhost/Final/Home/shop'
-                                    </script>";
-                        } 
-                    ?>
                     <button class="btn3" style="margin-left: 580px; background: transparent; margin-top: 1000px;">
                         <span class="btn-label">Lưu</span></button>
                 </form>
